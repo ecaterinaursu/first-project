bd67f1b1c23a62739798c3c3fddd93a6508e439d
Artemie <artiom.maslennicov@mail.ru>
2022-09-07 22:41:37 +0300

    Am facut gradient in js

--- gradient.js
+++ gradient.js
@@ -1,9 +1,9 @@
-const body = document.querySelector("body")
+const bodyInput = document.querySelector("body")
 const firstInput = document.querySelector(".first")
 const secondInput = document.querySelector(".second")
 
 function gradient() {
-    body.style.background = `linear-gradient(to right, ${firstInput.value}, ${secondInput.value})`
+    bodyInput.style.background = `linear-gradient(to right, ${firstInput.value}, ${secondInput.value})`
 }
 
 firstInput.addEventListener("input", gradient);
