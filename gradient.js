039bfb16f632700ec7abc988eac178691984bfa5
Artemie <artiom.maslennicov@mail.ru>
2022-09-08 09:49:18 +0300

    Am schimbat function cu arf in js

--- gradient.js
+++ gradient.js
@@ -1,9 +1,9 @@
-const bodyInput = document.querySelector("body")
+const body = document.querySelector("body")
 const firstInput = document.querySelector(".first")
 const secondInput = document.querySelector(".second")
 
-function gradient() {
-    bodyInput.style.background = `linear-gradient(to right, ${firstInput.value}, ${secondInput.value})`
+const gradient = () => {
+body.style.background =`linear-gradient(to right, ${firstInput.value}, ${secondInput.value})`
 }
 
 firstInput.addEventListener("input", gradient);
