c1970cc4b9e3b42491f6c36936767e56639329bc
Artemie <artiom.maslennicov@mail.ru>
2022-09-08 12:58:03 +0300

    Am facut arf inloc de function in js

--- gradient.js
+++ gradient.js
@@ -3,7 +3,7 @@ const firstInput = document.querySelector(".first")
 const secondInput = document.querySelector(".second")
 
 const gradient = () => {
-body.style.background =`linear-gradient(to right, ${firstInput.value}, ${secondInput.value})`
+    body.style.background =`linear-gradient(to right, ${firstInput.value}, ${secondInput.value})`
 }
 
 firstInput.addEventListener("input", gradient);
